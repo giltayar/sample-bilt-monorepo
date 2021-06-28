b8f26fc0981173601d8aaf98ad9ad28f946d1081
Gil Tayar <gil@tayar.org>
2021-06-28 07:18:54 +0300

    try out 2

--- packages/promise-utils/test/unit/promise-utils.unit.test.js
+++ packages/promise-utils/test/unit/promise-utils.unit.test.js
@@ -4,7 +4,7 @@ import {expect} from 'chai'
 
 import * as m from '../../src/promise-utils.js'
 
-describe('promise-utils (unit)', function () {
+describe('promise-utils (unit) ', function () {
   it('should be able to load (you can delete this test once you have others)', async () => {
     expect(m).to.not.be.undefined
   })
